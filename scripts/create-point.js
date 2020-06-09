6595f999ea973c68fe6fc4f8f8594330d29f3098
Lucas Rafael Ricci <lucasrafael.ricci@gmail.com>
2020-06-09 13:01:20 -0300

    Terceiro dia - Segunda página | interação com os itens de coleta via CSS e JS

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,3 +1,8 @@
+const states = document.querySelector('[name="uf"]');
+const itemsToCollect = document.querySelectorAll('.items-grid li');
+const inputItems = document.querySelector('.items-grid input');
+const selectedItems = [];
+
 function populateUFs() {
   const ufSelect = document.querySelector('[name="uf"]');
   const url = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
@@ -5,7 +10,7 @@ function populateUFs() {
   fetch(url)
     .then(res => res.json())
     .then(states => {
-      for(const state of states)
+      for (const state of states)
         ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
     });
 }
@@ -18,18 +23,36 @@ function getCities(e) {
   const indexOfSelectedState = e.target.selectedIndex;
 
   stateInput.value = e.target[indexOfSelectedState].textContent;
+  citySelect.innerHTML = `<option>Selecione a cidade</option>`;
+  citySelect.disabled = true;
 
   fetch(url)
     .then(res => res.json())
     .then(cities => {
-      for(const city of cities)
-        citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
+      for (const city of cities)
+        citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`;
     });
-  citySelect.disabled = false
+  citySelect.disabled = false;
+}
+
+function handleSelectedItems(e) {
+  const itemId = e.target.dataset.id;
+  const itemLi = e.target;
+  const alreadySelected = selectedItems.indexOf(itemId);
+  const itemFound = alreadySelected !== -1;
+
+  itemLi.classList.toggle('selected');
+
+  if (itemFound) {
+    selectedItems.splice(alreadySelected, 1);
+    return inputItems.value = selectedItems;
+  }
+  selectedItems.push(itemId);
+  inputItems.value = selectedItems;
 }
 
 populateUFs();
+states.addEventListener('change', getCities);
 
-document
-  .querySelector('[name="uf"]')
-  .addEventListener('change', getCities)
+for (const item of itemsToCollect)
+  item.addEventListener('click', handleSelectedItems);
