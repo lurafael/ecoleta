fc9484b84deee88931f771082d4fb36496eb40b6
Lucas Rafael Ricci <lucasrafael.ricci@gmail.com>
2020-06-14 12:31:22 -0300

    Finalizando o projeto com o banco de dados interagindo com a página colocando informações dinamicamente, também adicionada uma página de sucesso ao cadastrar um ponto de coleta

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const server = express();
 const nunjucks = require('nunjucks');
+const db = require("./database/db");
 
 server.use(express.static("public"));
+server.use(express.urlencoded({ extended: true }));
 
 nunjucks.configure('src/views', {
   express: server,
@@ -17,8 +19,53 @@ server.get("/create-point", (req, res) => {
   return res.render('create-point.html');
 });
 
+server.post("/savepoint", (req, res) => {
+  const query = `
+    INSERT INTO places (
+      image,
+      name,
+      address,
+      address2,
+      state,
+      city,
+      items
+    ) VALUES (?,?,?,?,?,?,?);
+  `;
+
+  const values = [
+    req.body.image,
+    req.body.name,
+    req.body.address,
+    req.body.address2,
+    req.body.state,
+    req.body.city,
+    req.body.items
+  ];
+
+  db.run(query, values, afterInsertData);
+
+  function afterInsertData(err) {
+    if (err)
+      return console.log(err);
+  return res.render('sucess.html');
+  }
+});
+
 server.get("/search-results", (req, res) => {
-  return res.render('search-results.html');
+  const search = req.query.search;
+  if (search === '')
+    return res.render('search-results.html', { totalPlaces: 0 });
+
+  db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function (err, rows) {
+    const totalPlaces = rows.length;
+
+    if (err)
+      return console.log(err);
+    return res.render('search-results.html', {
+      places: rows,
+      totalPlaces: totalPlaces
+    });
+  });
 });
 
 server.listen(3000);
